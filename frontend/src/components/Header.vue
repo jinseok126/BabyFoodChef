d21d6bade5d89ff7ce029114e0d0b85b8e931c32
DunDung <ebseud6135@naver.com>
2019-11-07 02:04:41 +0900

    20191107
    2

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,37 +1,38 @@
 <template>
-  <div>
-    <div class="momo">
-      <b-nav small align="right" style="border-bottom: 1px solid gray" >
-        <b-nav-item >로그인</b-nav-item>
-        <b-nav-item to="register">회원가입</b-nav-item>
-        <b-nav-item-dropdown text="정보 찾기" right>
-          <b-dropdown-item href="#" >아이디 찾기</b-dropdown-item>
-          <b-dropdown-item href="#">비밀번호 찾기</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-nav>
-    </div>
-    <div class="logo">
-      <b-navbar variant="faded" type="center">
-        <b-navbar-brand  href="/">
-          <h2>이유식 요리사</h2>
-        </b-navbar-brand>
-      </b-navbar>
-    </div>
-
-    <div class="nav-item">
-      <b-nav tabs justified >
-          <!--우리가 만들 라우터를 이용하기 위해선 href가 아닌 to로? -->
-          <b-nav-item-dropdown text="레시피" right active style="color: black">
-            <b-dropdown-item href="#">초기</b-dropdown-item>
-            <b-dropdown-item href="#">중기</b-dropdown-item>
-            <b-dropdown-item href="#">후기</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item to="#" active>회원 공유 레시피</b-nav-item>
-          <b-nav-item to="#" active>정보</b-nav-item>
-          <b-nav-item to="#" active>자유 게시판</b-nav-item>
-        </b-nav>
+  <body>
+      <div id="wrap">
+          <header id="header">
+              <div class="container">
+                  <div class="top_menu">
+                      <router-link to="/">로그인</router-link>
+                      <router-link to="register">회원가입</router-link>
+                      <a href="#">정보 찾기</a>
+                  </div>
+                  <h1><a href="#">이유식 요리사</a></h1>
+              </div>
+          </header>
+          <main>
+              <nav class="gnb">
+                  <h2>GNB</h2>
+                  <ul>
+                      <li><a href="#">레시피</a>
+                          <p>레시피 내용</p>
+                      </li>
+                      <li><a href="#">회원공유레시피</a>
+                          <p>회원공유레시피 내용</p>
+                      </li>
+                      <li><a href="#">정보</a>
+                          <p>정보 내용</p>
+                      </li>
+                      <li><a href="#">자유게시판</a>
+                          <p>자유게시판 내용</p>
+                      </li>
+                  </ul>
+              </nav>
+          </main>
+          <footer></footer>
       </div>
-  </div>
+  </body>
 </template>
 
 <script>
@@ -45,21 +46,34 @@ export default {
 }
 </script>
 <style scoped>
-.momo {
-  height: 15px;
-}
-.logo {
-  display: inline-block; /*가운데 정렬*/
-  float: none;
-}
-h2 {
-  color: #2F3B52;
-  font-weight: 900;
-  margin: 2.0rem 0 1.5rem;
-}
-.nav-item {
-  font-weight: 900;
-  color: #FFFFFF;
-}
+body,h1,h2,h3,h4,h5,h6,p,div,header,main,footer,section,article,nav,ul,li,form,fieldset,legend,label,p,address,table,dl,dt,dd,input,select,textarea,button,figure,figcaption{margin:0;padding:0;font-family:'Noto Sans KR Light',sans-serif}
 
+     ul,li{list-style:none}
+     a{text-decoration:none;color:#222}
+     a:hover{color:#2698cb}
+     address{font-style:normal}
+     form,input,button{vertical-align:middle}
+     button{cursor:pointer}
+     img,fieldset{border:none}
+     input{border:none;outline:none}
+
+     body,input,textarea,select,button,table{font-family:"Noto Sana KR",sans-serif;font-size:13px;line-height:1.5;letter-spacing:-1px;direction:ltr}
+
+     /*header*/
+     #header{height:175px;background:url(../../static/images/top_bg.jpg) repeat-x}
+     .container{width:1200px;margin:0 auto}
+     .top_menu{text-align:right}
+     .top_menu>a{display:inline-block;padding:9px 10px 18px 10px;transition:color 0.3s ease;font-size:15px}
+     .top_menu>a:hover{color:#fff}
+     #header h1{width:183px;height:118px;background:url(../../static/images/cook_logo.jpg) no-repeat;text-indent:-9999px}
+     #header h1>a{display:block;height:100%;}
+
+     /*gnb*/
+     .gnb{border-top:1px solid #ddd;border-bottom:1px solid #ddd}
+     .gnb h2{position:absolute;left:-9999px;top:-9999px;}
+     .gnb>ul{width:1200px;margin:0 auto;overflow:hidden}
+     .gnb>ul>li{width:299px;height:50px;float:left;font-size:18px;font-family:'Noto Sans KR Regular',sans-serif;text-align:center}
+     .gnb>ul>li>a{transition:color 0.3s ease;display:inline-block;padding-top:10px}
+     .gnb>ul>li>a:hover{color:#85b729}
+     .gnb p{display:none}
 </style>
