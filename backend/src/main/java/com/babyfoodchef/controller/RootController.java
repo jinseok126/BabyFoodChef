489a09bccd931e6fecc64f76a51a961a94fd4649
JunSeong <ebseud6135@naver.com>
2019-11-11 19:08:15 +0900

    Revert "Revert "20191107""
    This reverts commit 9420b9a00201831f1d5bb5299b44c97d927ade59.

--- backend/src/main/java/com/babyfoodchef/controller/RootController.java
+++ backend/src/main/java/com/babyfoodchef/controller/RootController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RootController implements ErrorController {
-    @GetMapping("/error")
+    @GetMapping({"/","/error"})
     public String getUsers(){
         return "index";
     }
