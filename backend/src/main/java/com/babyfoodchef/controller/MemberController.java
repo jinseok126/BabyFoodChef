23e3b8c35e86df346e1835e532fc973f2a9a6dce
JunSeong <ebseud6135@naver.com>
2019-11-16 01:18:39 +0900

    20191116

--- backend/src/main/java/com/babyfoodchef/controller/MemberController.java
+++ backend/src/main/java/com/babyfoodchef/controller/MemberController.java
@@ -26,20 +26,20 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public void login(@RequestBody LoginObjDto loginObj, HttpServletResponse response) {
+    public String login(@RequestBody LoginObjDto loginObj, HttpServletResponse response) {
         MemberDto member = memberService.findByIdAndPw(loginObj);
         if (member==null) return;
         TokenDto tokenDto = jwtService.createTokenDto(member);
-        response.addHeader("Token", tokenDto.getAccessToken());
         member.setToken(tokenDto.getRefreshToken());
         memberService.update(member);
+        return tokenDto.getAccessToken();
     }
     @GetMapping("/findById/{id}")
-    public void sendMemberInfo(@PathVariable String id, HttpServletResponse response){
+    public String findById(@PathVariable String id, HttpServletResponse response){
         //헤더에 닉네임을 보냄
         //나중에 보낼게 더 있으면 토큰에다가 추가해서 보내기.
         MemberDto member = memberService.findById(id);
-        response.addHeader("NickName", member.getNickName());
+        return member.getNickName();
     }
     //return으로 vue에 전달하기 테스트해보기, 객체로 뷰에 전달하기 테스트
     @GetMapping("/findByEmail/{email}")
