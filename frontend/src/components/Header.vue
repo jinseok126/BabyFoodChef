54796f75589cabfb6fce375d4ce14dde5a184655
iamkjw77 <45993668+iamkjw77@users.noreply.github.com>
2019-11-16 23:54:55 +0900

    2019_11_16

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <body>
+  <div class="body">
       <div id="wrap">
-          <header id="header">
+          <div id="header">
               <div class="container">
                   <div class="top_menu">
                       <router-link to="login" v-show="isLogin===false">로그인</router-link>
@@ -12,30 +12,45 @@
                       <router-link to="/" v-show="isLogin===false">비밀번호 찾기</router-link>
                   </div>
                   <h1><router-link to="/">이유식 요리사</router-link></h1>
+                  <div class="gnb">
+                    <h2>GNB</h2>
+                    <ul>
+                      <li class="service"><a href="#">이요 서비스</a>
+                        <ul>
+                          <li><a href="#">이요 사용법</a></li>
+                          <li><a href="#">이유식 정보</a></li>
+                          <li><a href="#">사이트맵</a></li>
+                        </ul>
+                      </li>
+                      <li class="recommend"><a href="#">추천식단</a>
+                        <ul>
+                          <li><a href="#">초기(5~6개월)</a></li>
+                          <li><a href="#">중기(7~8개월)</a></li>
+                          <li><a href="#">후기(9~11개월)</a></li>
+                          <li><a href="#">완료기(12~14개월)</a></li>
+                        </ul>
+                      </li>
+                      <li class="talk"><a href="#">맘's 토크</a>
+                        <ul>
+                          <li><a href="#">공지사항</a></li>
+                          <li><a href="#">나만의 레시피 공유</a></li>
+                          <li><a href="#">자유 게시판</a></li>
+                        </ul>
+                      </li>
+                      <li class="event"><a href="#">이벤트</a>
+                        <ul>
+                          <li><a href="#">진행중인 이벤트</a></li>
+                          <li><a href="#">종료된 이벤트</a></li>
+                          <li><a href="#">당첨자 발표</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
               </div>
-          </header>
-          <main>
-              <nav class="gnb">
-                  <h2>GNB</h2>
-                  <ul>
-                      <li><a href="#">레시피</a>
-                          <p>레시피 내용</p>
-                      </li>
-                      <li><a href="#">회원공유레시피</a>
-                          <p>회원공유레시피 내용</p>
-                      </li>
-                      <li><a href="#">정보</a>
-                          <p>정보 내용</p>
-                      </li>
-                      <li><a href="#">자유게시판</a>
-                          <p>자유게시판 내용</p>
-                      </li>
-                  </ul>
-              </nav>
-          </main>
-          <footer></footer>
+              <div class="gnb_bg"></div>
+          </div>
       </div>
-  </body>
+  </div>
 </template>
 
 <script>
@@ -54,34 +69,28 @@ export default {
 </script>
 <!--scoped = 해당 컴포넌트에만 style 적용 -->
 <style scoped>
-body,h1,h2,h3,h4,h5,h6,p,div,header,main,footer,section,article,nav,ul,li,form,fieldset,legend,label,p,address,table,dl,dt,dd,input,select,textarea,button,figure,figcaption{margin:0;padding:0;font-family:'Noto Sans KR Light',sans-serif}
-
-     ul,li{list-style:none}
-     a{text-decoration:none;color:#222}
-     a:hover{color:#2698cb}
-     address{font-style:normal}
-     form,input,button{vertical-align:middle}
-     button{cursor:pointer}
-     img,fieldset{border:none}
-     input{border:none;outline:none}
-
-     body,input,textarea,select,button,table{font-family:"Noto Sana KR",sans-serif;font-size:13px;line-height:1.5;letter-spacing:-1px;direction:ltr}
-
      /*header*/
-     #header{height:175px;background:url("/static/images/top_bg.jpg") repeat-x}
+     #header{height:150px;background:url('/static/images/top_bg.jpg') repeat-x}
      .container{width:1200px;margin:0 auto}
-     .top_menu{text-align:right}
-     .top_menu>a{display:inline-block;padding:9px 10px 18px 10px;transition:color 0.3s ease;font-size:15px}
-     .top_menu>a:hover{color:#fff}
-     #header h1{width:183px;height:118px;background:url("/static/images/cook_logo.jpg") no-repeat;text-indent:-9999px}
+     .top_menu{text-align:right;height:42px;}
+     .top_menu>a{display:inline-block;color:#fff;font-size:15px;line-height:40px;padding-right:15px}
+     #header h1{float:left;width:280px;height:80px;background:url('/static/images/logo.jpg') no-repeat;text-indent:-9999px;margin-top:20px}
      #header h1>a{display:block;height:100%;}
 
      /*gnb*/
-     .gnb{border-top:1px solid #ddd;border-bottom:1px solid #ddd}
+     .gnb{float:left;width:920px;position:relative;z-index:10}
      .gnb h2{position:absolute;left:-9999px;top:-9999px;}
-     .gnb>ul{width:1200px;margin:0 auto;overflow:hidden}
-     .gnb>ul>li{width:299px;height:50px;float:left;font-size:18px;font-family:'Noto Sans KR Regular',sans-serif;text-align:center}
-     .gnb>ul>li>a{transition:color 0.3s ease;display:inline-block;padding-top:10px}
-     .gnb>ul>li>a:hover{color:#85b729}
-     .gnb p{display:none}
+     .gnb>ul{overflow:hidden}
+     .gnb>ul>li>a{float:left;display:block;width:230px;height:70px;font-size:20px;font-family:'Noto Sans KR Regular',sans-serif;text-align:center;line-height:70px;padding:20px 0 20px 0}
+     .gnb>ul>li>a:hover{color:#85b729;background:url('/static/images/gnb_m_over_bar.jpg') no-repeat center bottom}
+
+     .gnb ul ul{display:none;z-index:10;width:230px;height:200px;}
+     .gnb ul ul li{text-align:center;font-size:17px;padding:10px}
+     .gnb .service ul{position:absolute;left:0px;top:109px;}
+     .gnb .recommend ul{position:absolute;left:230px;top:109px;}
+     .gnb .talk ul{position:absolute;left:460px;top:109px;}
+     .gnb .event ul{position:absolute;left:690px;top:109px;}
+     .gnb ul ul li a:hover{color:#85b729}
+
+     .gnb_bg{background:#fff;width:100%;height:200px;border-top:1px solid #eee;display:none;position:absolute;left:0;top:150px;z-index:1}
 </style>
