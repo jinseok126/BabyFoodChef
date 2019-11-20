6750c6eeac5091dab61df8f0bad6d6e238b57d62
iamkjw77 <45993668+iamkjw77@users.noreply.github.com>
2019-11-21 02:11:10 +0900

    2019_11_21

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,11 +5,9 @@
               <div class="container">
                   <div class="top_menu">
                       <router-link to="login" v-show="isLogin===false">로그인</router-link>
+                      <router-link to="register" v-show="isLogin===false">회원가입</router-link>
                       <router-link to="myPage" v-show="isLogin">마이페이지</router-link>
                       <button v-show="isLogin" @click="logout">로그아웃</button>
-                      <router-link to="register" v-show="isLogin===false">회원가입</router-link>
-                      <router-link to="findId" v-show="isLogin===false">아이디 찾기</router-link>
-                      <router-link to="/" v-show="isLogin===false">비밀번호 찾기</router-link>
                   </div>
                   <div class="gnb">
                     <h1><router-link to="/">이유식 요리사</router-link></h1>
@@ -69,27 +67,39 @@ export default {
 </script>
 <!--scoped = 해당 컴포넌트에만 style 적용 -->
 <style scoped>
+    body,h1,h2,h3,h4,h5,h6,p,div,header,main,footer,section,article,nav,ul,li,form,fieldset,legend,label,p,address,table,dl,dt,dd,input,select,textarea,button,figure,figcaption{margin:0;padding:0;font-family:'Noto Sans KR Light',sans-serif}
+
+    ul,li{list-style:none}
+    a{text-decoration:none;color:#222}
+    a:hover{color:#2698cb}
+    address{font-style:normal}
+    form,input,button{vertical-align:middle}
+    button{cursor:pointer}
+    img,fieldset{border:none}
+    input{border:none;outline:none}
+
+    body,input,textarea,select,button,table{font-family:"Noto Sana KR",sans-serif;font-size:13px;line-height:1.5;letter-spacing:-1px;direction:ltr}
      /*header*/
      #header{height:150px;background:url('/static/images/top_bg.jpg') repeat-x}
      .container{width:1200px;margin:0 auto}
      .top_menu{text-align:right;height:42px;}
-     .top_menu>a{display:inline-block;color:#fff;font-size:15px;line-height:40px;padding-right:15px}
-     #header h1{float:left;width:280px;height:80px;background:url('/static/images/logo.jpg') no-repeat;text-indent:-9999px;margin-top:20px}
-     #header h1>a{display:block;height:100%;}
+     .top_menu>a{display:inline-block;color:#fff;font-size:15px;line-height:40px;padding-right:30px}
 
      /*gnb*/
-     .gnb{float:left;width:1200px;position:relative;z-index:10}
+     .gnb{width:1200px;position:relative;z-index:10}
+     .gnb h1{float:left;width:280px;height:80px;background:url('/static/images/logo.jpg') no-repeat;text-indent:-9999px;margin-top:20px}
+     .gnb h1>a{display:block;height:100%;}
      .gnb h2{position:absolute;left:-9999px;top:-9999px;}
      .gnb>ul{overflow:hidden}
      .gnb>ul>li>a{float:left;display:block;width:230px;height:70px;font-size:20px;font-family:'Noto Sans KR Regular',sans-serif;text-align:center;line-height:70px;padding:20px 0 20px 0}
      .gnb>ul>li>a:hover{color:#85b729;background:url('/static/images/gnb_m_over_bar.jpg') no-repeat center bottom}
 
-     .gnb ul ul{display:none;z-index:10;width:230px;height:200px;}
+     .gnb ul ul{display:none;z-index:10;width:230px;height:auto;}
      .gnb ul ul li{text-align:center;font-size:17px;padding:10px}
-     .gnb .service ul{position:absolute;left:0px;top:109px;}
-     .gnb .recommend ul{position:absolute;left:230px;top:109px;}
-     .gnb .talk ul{position:absolute;left:460px;top:109px;}
-     .gnb .event ul{position:absolute;left:690px;top:109px;}
+     .gnb .service ul{position:absolute;left:280px;top:109px;}
+     .gnb .recommend ul{position:absolute;left:510px;top:109px;}
+     .gnb .talk ul{position:absolute;left:740px;top:109px;}
+     .gnb .event ul{position:absolute;left:970px;top:109px;}
      .gnb ul ul li a:hover{color:#85b729}
 
      .gnb_bg{background:#fff;width:100%;height:200px;border-top:1px solid #eee;display:none;position:absolute;left:0;top:150px;z-index:1}
--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="form">
+  <!--<div class="form">
     <b-card bg-variant="light">
       <b-form-group
       label-cols-lg="4"
@@ -56,7 +56,52 @@
      <b-button class="button" @click="userRegister" variant="info">회원 가입</b-button>
    </b-form-group>
   </b-card>
-  </div>
+</div>-->
+<div id="main">
+    <form action="" name="join_form" method="post">
+        <fieldset>
+            <legend class="sign_up_title">회원가입</legend>
+            <p class="sign_up_sub_title">이유식 요리사를 100% 즐기는 방법, 가입을 환영합니다!</p>
+            <div class="form_wrap">
+                <p class="uid">
+                    <label for="uid">아이디</label>
+                    <input type="text" name="uid" id="uid" size="20" />
+                    <button type="">중복확인</button>
+                </p>
+                <div class="pwd_wrap">
+                    <p class="pwd">
+                        <label for="pwd">비밀번호</label>
+                        <input type="password" name="pwd" id="pwd" size="59" />
+                    </p>
+                    <p class="repwd">
+                        <label for="repwd">비밀번호 확인</label>
+                        <input type="password" name="repwd" id="repwd" size="59" />
+                    </p>
+                    <p class="pwd_caution">최소 6자 이상 13자 이하로 작성해주세요.</p>
+                </div>
+                <p class="nick">
+                    <label for="nick">닉네임</label>
+                    <input type="text" name="nick" id="nick" size="20" />
+                    <button type="">중복확인</button>
+                </p>
+                <p class="uemail">
+                    <label for="email_id">이메일</label>
+                    <input type="text" name="email_id" id="email_id" size="10" /> @
+                    <input type="text" name="email_dns" id="email_dns" class="email_dns" size="18" />
+                    <select name="sel_dns" id="sel_dns" onchange="">
+                        <option value="">직접입력</option>
+                        <option value="naver.com">네이버</option>
+                        <option value="hanmail.net">다음</option>
+                        <option value="gmail.com">구글</option>
+                    </select>
+                </p>
+                <p class="join_button">
+                    <button type="button" onclick="form_check(this.form)">가입하기</button>
+                </p>
+            </div>
+        </fieldset>
+    </form>
+</div>
 </template>
 <script>
 import axios from 'axios'
@@ -115,7 +160,7 @@ export default {
 }
 </script>
 <style scoped>
-.form {
+/*.form {
   border: 1px solid black;
   margin-top: 50px;
   margin-bottom: 50px;
@@ -125,5 +170,42 @@ export default {
 .button {
   margin-top: 20px;
   margin-left: 180px;
-}
+}*/
+body,h1,h2,h3,h4,h5,h6,p,div,header,main,footer,section,article,nav,ul,li,form,fieldset,legend,label,p,address,table,dl,dt,dd,input,select,textarea,button,figure,figcaption{margin:0;padding:0;font-family:'Noto Sans KR Light',sans-serif}
+
+ul,li{list-style:none}
+a{text-decoration:none;color:#222}
+a:hover{color:#2698cb}
+address{font-style:normal}
+form,input,button{vertical-align:middle}
+button{cursor:pointer}
+img,fieldset{border:none}
+input{border:none;outline:none}
+
+body,input,textarea,select,button,table{font-family:"Noto Sana KR",sans-serif;font-size:13px;line-height:1.5;letter-spacing:-1px;direction:ltr}
+
+/*main*/
+#main{background:#f7f7f5;height:900px;}
+.sign_up_title{font-size:36px;font-family:'Noto Sans KR Regular',sans-serif;text-align:center;padding-top:40px}
+.sign_up_sub_title{font-size:17px;font-family:'Noto Sans KR Regular',sans-serif;text-align:center;padding-top:10px;margin-bottom:25px}
+.form_wrap{position:relative;width:675px;height:550px;background:#fff;margin:0 auto;padding:40px}
+.form_wrap label{position:absolute;left:41px;font-size:18px;color:#9e9e9e;font-family:'Noto Sans KR Regular',sans-serif;cursor:text}
+.form_wrap input{border-bottom:1px solid #d0d0d0;font-size:18px;font-family:'Noto Sans KR Regular',sans-serif;margin-bottom:15px;padding-top:10px;padding:13px 0px;}
+.form_wrap .uid label{top:55px}
+.form_wrap .pwd label{top:125px}
+.form_wrap .repwd label{top:195px}
+.form_wrap .nick label{top:285px}
+.form_wrap .uemail label{top:350px}
+.form_wrap .email_dns{padding-left:10px}
+.form_wrap .uid button{background:#ffa200;border:none;color:#fff;padding:10px 10px;width:100px;border-radius:8px;margin-left:10px}
+.form_wrap .uid button:hover{background:#ef8700}
+.form_wrap .nick button{background:#ffa200;border:none;color:#fff;padding:10px 10px;width:100px;border-radius:8px;margin-left:10px}
+.form_wrap .nick button:hover{background:#ef8700}
+.form_wrap .repwd input{margin-bottom:5px}
+.form_wrap .pwd_caution{padding-left:25px;height:20px;background:url('/static/images/sign_up_caution.jpg') no-repeat;margin-bottom:10px}
+.form_wrap .uemail{margin-bottom:35px}
+.form_wrap .uemail select{-webkit-appearance:none;-moz-appearance:none;appearance:none;width:175px;padding:10px;background:url('/static/images/select_icon.jpg') no-repeat 140px 17px;color:#9e9e9e;font-size:17px;font-family:'Noto Sans KR Regular',sans-serif;margin-left:14px}
+.form_wrap .join_button{text-align:center}
+.form_wrap .join_button button{background:#85b729;border:none;color:#fff;padding:15px 45px;border-radius:8px;font-size:18px;font-family:'Noto Sans KR Regular',sans-serif;}
+.form_wrap .join_button button:hover{background:#6c9f0d;}
 </style>
