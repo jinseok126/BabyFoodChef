9dd9b2e018a643c192ab2e1829a3a570582b8405
iamkjw77 <iamkjw77@naver.com>
2020-01-22 01:59:42 +0900

    메인페이지
    메인이미지,컨텐츠 가운데 정렬,

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -3,83 +3,85 @@
     <!--main_image-->
     <div class="main_images">
       <ul>
-        <li class="main_image_01"><a href="#"><img src='/static/images/main_image_01.jpg' alt="이유식 요리사 오픈 기념 아기사진 공모전 이벤트"></a></li>
+        <li class="main_image_01"><a href="#"></a></li>
       </ul>
     </div>
-    <!--board-->
-    <div class="board">
-        <h2>게시판</h2>
-        <div class="notice">
-            <h3 clsss="sel"><a href="#">공지사항</a></h3>
-            <table width="600">
-                <caption>공지사항</caption>
-                <colgroup>
-                    <col width="440" />
-                    <col width="160" />
-                </colgroup>
-                <tbody>
-                    <tr>
-                        <td><a href="#">세 번째 게시물입니다.</a></td>
-                        <td align="center">2020.01.07</td>
-                    </tr>
-                    <tr>
-                        <td><a href="#">두 번째 게시물입니다.</a></td>
-                        <td align="center">2020.01.07</td>
-                    </tr>
-                    <tr>
-                        <td><a href="#">세 번째 게시물입니다.</a></td>
-                        <td align="center">2020.01.07</td>
-                    </tr>
-                </tbody>
-            </table>
+    <div class="contents_wrap">
+        <!--board-->
+        <div class="board">
+            <h2>게시판</h2>
+            <div class="notice">
+                <h3 clsss="sel"><a href="#">공지사항</a></h3>
+                <table width="600">
+                    <caption>공지사항</caption>
+                    <colgroup>
+                        <col width="440" />
+                        <col width="160" />
+                    </colgroup>
+                    <tbody>
+                        <tr>
+                            <td><a href="#">세 번째 게시물입니다.</a></td>
+                            <td align="center">2020.01.07</td>
+                        </tr>
+                        <tr>
+                            <td><a href="#">두 번째 게시물입니다.</a></td>
+                            <td align="center">2020.01.07</td>
+                        </tr>
+                        <tr>
+                            <td><a href="#">세 번째 게시물입니다.</a></td>
+                            <td align="center">2020.01.07</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="free_board">
+                <h3><a href="#">자유게시판</a></h3>
+                <table width="600">
+                    <caption>자유게시판</caption>
+                    <colgroup>
+                        <col width="440" />
+                        <col width="160" />
+                    </colgroup>
+                    <tbody>
+                        <tr>
+                            <td><a href="#">세 번째 게시물입니다.</a></td>
+                            <td align="center">2020.01.07</td>
+                        </tr>
+                        <tr>
+                            <td><a href="#">두 번째 게시물입니다.</a></td>
+                            <td align="center">2020.01.07</td>
+                        </tr>
+                        <tr>
+                            <td><a href="#">세 번째 게시물입니다.</a></td>
+                            <td align="center">2020.01.07</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <div class="free_board">
-            <h3><a href="#">자유게시판</a></h3>
-            <table width="600">
-                <caption>자유게시판</caption>
-                <colgroup>
-                    <col width="440" />
-                    <col width="160" />
-                </colgroup>
-                <tbody>
-                    <tr>
-                        <td><a href="#">세 번째 게시물입니다.</a></td>
-                        <td align="center">2020.01.07</td>
-                    </tr>
-                    <tr>
-                        <td><a href="#">두 번째 게시물입니다.</a></td>
-                        <td align="center">2020.01.07</td>
-                    </tr>
-                    <tr>
-                        <td><a href="#">세 번째 게시물입니다.</a></td>
-                        <td align="center">2020.01.07</td>
-                    </tr>
-                </tbody>
-            </table>
+        <!--recommend_meal-->
+        <div class="recommend_meal">
+            <h2>추천식단</h2>
+            <ul>
+                <li>초기(5~6개월)</li>
+                <li>중기(7~8개월)</li>
+                <li>후기(9~11개월)</li>
+                <li>완료기(12~24개월)</li>
+            </ul>
         </div>
-    </div>
-    <!--recommend_meal-->
-    <div class="recommend_meal">
-        <h2>추천식단</h2>
-        <ul>
-            <li>초기(5~6개월)</li>
-            <li>중기(7~8개월)</li>
-            <li>후기(9~11개월)</li>
-            <li>완료기(12~24개월)</li>
-        </ul>
-    </div>
-    <!--our_recipe_share-->
-    <div class="our_recipe_share">
-        <h2>나만의 레시피 공유</h2>
-        <p></p>
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <a href="#">이전보기</a>
-        <a href="#">다음보기</a>
+        <!--our_recipe_share-->
+        <div class="our_recipe_share">
+            <h2>나만의 레시피 공유</h2>
+            <p></p>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <a href="#">이전보기</a>
+            <a href="#">다음보기</a>
+        </div>
     </div>
   </div>
 </template>
@@ -90,8 +92,10 @@
   #main{}
     /*main_image*/
   .main_images{}
-  .main_image_01 img{}
+  .main_images .main_image_01{width:100%;height:400px;background:url('/static/images/main_image_01.jpg') no-repeat center}
+  .main_images .main_image_01 a{display:block;height:100%;}
     /*board*/
+  .contents_wrap{width:1200px;margin:0 auto}
   .board{float:left;width:600px;height:340px;border:1px solid #eee;position:relative}
   .board h2{position:absolute;left:-9999px;top:-9999px}
   .board .notice{}
